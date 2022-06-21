14ff4c95bdfe8cd93d4f4c713f2349011ec94c61
Iago <iagoassis21@outlook.com>
2022-06-21 16:46:14 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <SolarSystem />
       </div>
     );
   }
